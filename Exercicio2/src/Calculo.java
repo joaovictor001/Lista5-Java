cf57384c6a2018374b2fef4b68d9ade1077237aa
joaovictor001 <joaocarvalhoborges@outlook.com>
2024-05-08 11:19:50 -0300

    Exercicio 2

--- Exercicio2/src/Calculo.java
+++ Exercicio2/src/Calculo.java
@@ -1,21 +1,27 @@
 public class Calculo {
-    private Double descontoInss(double salario){
+    public Double descontoInss(double salario){
         salario = salario * 0.20;
         return salario;
 
     }
-    private Double descontoIrpf(double salario){
+    public Double descontoIrpf(double salario){
         salario = salario * 0.10;
         return salario;
-
     }
-    private Double descontoPlanoDeSaude(double salario){
+    public Double descontoPlanoDeSaude(double salario){
         salario = salario * 0.05;
         return salario;
-
     }
     public Double horasExtras(double horas,double salarioBruto){
-        return 0.0;
+        if (horas > 160){
+            double horasAmais = horas -160;
+            return horasAmais * (salarioBruto/2);
+        }return  0.0;
+//        double valorHora = salarioBruto /160;
+//        double horasPagar = 160  -  horas;
+//        double descontoHoras =  horasPagar * valorHora;
+//        System.out.println("Está devendo: " +horasPagar+" horas");
+//        return descontoHoras;
 
 
 
@@ -23,9 +29,9 @@ public class Calculo {
     public double salarioLiquido(double salario, double inss,double irpf,double plano,double horaExtra){
 
         salario = salario -inss -irpf -plano + horaExtra;
-        EntradaSaida salario_liquido = new EntradaSaida();
-        salario_liquido.setSalarioLiquido(salario);
         return salario;
 
     }
+
+
 }
--- Exercicio2/src/Main.java
+++ Exercicio2/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.print("Informe seu salario bruto para calcular os descontos: ");
+        double salarioBruto = sc.nextDouble();
+        System.out.print("Horas Trabalhadas: ");
+        double horaTrabalhadas = sc.nextDouble();
+
+        EntradaSaida entradaSaida = new EntradaSaida(salarioBruto);
+        Calculo calculo = new Calculo();
+
+        double inss = calculo.descontoInss(salarioBruto);
+        double irpf = calculo.descontoIrpf(salarioBruto);
+        double planoDeSaude = calculo.descontoPlanoDeSaude(salarioBruto);
+        double horaExtra = calculo.horasExtras(horaTrabalhadas,salarioBruto);
+
+        System.out.println(calculo.salarioLiquido(salarioBruto,inss,irpf,planoDeSaude,horaExtra));
+
+
+
+
     }
 }
